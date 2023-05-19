d80849ea70bff93d71545263839960decca71fb6
pravin9033 <singh.pravin1996@gmail.com>
2023-05-18 21:29:41 -0500

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
+import 'bulma/css/bulma.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    return (
+        <div>
+            <section className="hero is-primary">
+                <div className="hero-body"> 
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+            
+
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title ="Alexa" 
+                                handle ="@alexa123" 
+                                image = {AlexaImage}
+                                description ="hey this is Amazon Alexa! Nice to meet You"
+                            />
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title ="Cortana" 
+                                handle ="@cortana456" 
+                                image = {CortanaImage} 
+                                description ="hey this is Microsoft Cortana! Nice to meet You"
+                            />
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title ="Siri" 
+                                handle ="@siri789" 
+                                image = {SiriImage}
+                                description ="hey this is Apple Siri! Nice to meet You"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+            
+            
+            
+        </div>
+    );
 }
 
 export default App;
